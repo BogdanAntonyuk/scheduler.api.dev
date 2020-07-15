c8ed54b18241f8ed16f3b8d53346d3b943141541
BogdanAntonyuk <boba.tochka@gmail.com>
2020-07-15 18:08:49 +0300

    added updade after edit for "B" column

--- src/insertDataIntoSpreadSheets.js
+++ src/insertDataIntoSpreadSheets.js
@@ -1,78 +1,152 @@
 const { google } = require('googleapis');
+const keys = require('../config/keys.json');
 const getEpisode = require('./utils/getEpisode.js');
 const updateDescription = require('./utils/updateDescription.js');
 
+const client = new google.auth.JWT(keys.client_email, null, keys.private_key, [
+  'https://www.googleapis.com/auth/spreadsheets',
+]);
+
+const gsapi = google.sheets({ version: 'v4', auth: client });
 const regExp = new RegExp(/^[A-Z]{4}/, 'g');
 
-let insertDataFromDB2YourHopeIntoSpreadSheet = async function gsrun(cl, sheetID, sheetName) {
-	const gsapi = google.sheets({ version: 'v4', auth: cl });
+async function insertDataFromDB2YourHopeIntoSpreadSheet(sheetID, sheetName) {
+  const opt = {
+    spreadsheetId: sheetID,
+    range: sheetName + '!B1:B55',
+  };
+  let prNames = [];
+  let fetchedData = [];
+  let commercialBr = [];
+  let descr = [];
 
-	const opt = {
-		spreadsheetId: sheetID,
-		range: sheetName + '!B1:B55',
-	};
-	let prNames = [];
-	let fetchedData = [];
-	let commercialBr = [];
-	let descr = [];
+  const rawData = await gsapi.spreadsheets.values.get(opt);
+  const mediumData = rawData.data.values;
+  const lettersOnlyCodesFromSheet = mediumData.map(function (str1) {
+    return str1.map(function (str2) {
+      return str2.match(regExp)[0];
+    });
+  });
 
-	const rawData = await gsapi.spreadsheets.values.get(opt);
-	const mediumData = rawData.data.values;
-	const lettersOnlyCodesFromSheet = mediumData.map(function (str1) {
-		return str1.map(function (str2) {
-			return str2.match(regExp)[0];
-		});
-	});
+  for (i = 0; i < mediumData.length; i++) {
+    fetchedData.push(await getEpisode(mediumData[i])); // fetching object from http://db2.yourhope.tv/api/episodes/'
+    descr.push(await updateDescription(lettersOnlyCodesFromSheet[i])); // fetching object from http://db2.yourhope.tv/api/shows/
+  }
+  for (j = 0; j < fetchedData.length; j++) {
+    if (fetchedData[j].error) {
+      prNames.push(new Array(fetchedData[j].error.message));
+      commercialBr.push(new Array());
+    } else if (descr[j].error) {
+      descr[j].error.message;
+    } else if (!fetchedData[j].timeline_break) {
+      commercialBr.push(new Array());
+      prNames.push(
+        new Array(
+          fetchedData[j].show.title,
+          descr[j].description.short,
+          fetchedData[j].title,
+          fetchedData[j].duration
+        )
+      );
+    } else {
+      commercialBr.push(new Array(fetchedData[j].timeline_break));
+      prNames.push(
+        new Array(
+          fetchedData[j].show.title,
+          descr[j].description.short,
+          fetchedData[j].title,
+          fetchedData[j].duration
+        )
+      );
+    }
+  }
 
-	for (i = 0; i < mediumData.length; i++) {
-		fetchedData.push(await getEpisode(mediumData[i])); // fetching object from http://db2.yourhope.tv/api/episodes/'
-		descr.push(await updateDescription(lettersOnlyCodesFromSheet[i])); // fetching object from http://db2.yourhope.tv/api/shows/
-	}
-	for (j = 0; j < fetchedData.length; j++) {
-		if (fetchedData[j].error) {
-			prNames.push(new Array(fetchedData[j].error.message));
-			commercialBr.push(new Array());
-		} else if (descr[j].error) {
-			descr[j].error.message;
-		} else if (!fetchedData[j].timeline_break) {
-			commercialBr.push(new Array());
-			prNames.push(
-				new Array(
-					fetchedData[j].show.title,
-					descr[j].description.short,
-					fetchedData[j].title,
-					fetchedData[j].duration
-				)
-			);
-		} else {
-			commercialBr.push(new Array(fetchedData[j].timeline_break));
-			prNames.push(
-				new Array(
-					fetchedData[j].show.title,
-					descr[j].description.short,
-					fetchedData[j].title,
-					fetchedData[j].duration
-				)
-			);
-		}
-	}
+  const updateOptionsForPrNames = {
+    spreadsheetId: sheetID,
+    range: sheetName + '!D1',
+    valueInputOption: 'USER_ENTERED',
+    resource: { values: prNames },
+  };
 
-	const updateOptionsForPrNames = {
-		spreadsheetId: sheetID,
-		range: sheetName + '!D1',
-		valueInputOption: 'USER_ENTERED',
-		resource: { values: prNames },
-	};
+  const updateOptionsForCommercialBr = {
+    spreadsheetId: sheetID,
+    range: sheetName + '!A1',
+    valueInputOption: 'USER_ENTERED',
+    resource: { values: commercialBr },
+  };
 
-	const updateOptionsForCommercialBr = {
-		spreadsheetId: sheetID,
-		range: sheetName + '!A1',
-		valueInputOption: 'USER_ENTERED',
-		resource: { values: commercialBr },
-	};
+  await gsapi.spreadsheets.values.update(updateOptionsForPrNames);
+  await gsapi.spreadsheets.values.update(updateOptionsForCommercialBr);
+}
 
-	await gsapi.spreadsheets.values.update(updateOptionsForPrNames);
-	await gsapi.spreadsheets.values.update(updateOptionsForCommercialBr);
-};
+async function fetchAfterEdit(sheetID, sheetName, range) {
+  const opt = {
+    spreadsheetId: sheetID,
+    range: sheetName + '!' + range,
+  };
+  const row = range.slice(1);
+  let prNames = [];
+  let fetchedData = [];
+  let commercialBr = [];
+  let descr = [];
 
-module.exports = insertDataFromDB2YourHopeIntoSpreadSheet;
+  const rawData = await gsapi.spreadsheets.values.get(opt);
+  const mediumData = rawData.data.values;
+  const lettersOnlyCodesFromSheet = mediumData.map(function (str1) {
+    return str1.map(function (str2) {
+      return str2.match(regExp)[0];
+    });
+  });
+
+  for (i = 0; i < mediumData.length; i++) {
+    fetchedData.push(await getEpisode(mediumData[i])); // fetching object from http://db2.yourhope.tv/api/episodes/'
+    descr.push(await updateDescription(lettersOnlyCodesFromSheet[i])); // fetching object from http://db2.yourhope.tv/api/shows/
+  }
+  for (j = 0; j < fetchedData.length; j++) {
+    if (fetchedData[j].error) {
+      prNames.push(new Array(fetchedData[j].error.message));
+      commercialBr.push(new Array());
+    } else if (descr[j].error) {
+      descr[j].error.message;
+    } else if (!fetchedData[j].timeline_break) {
+      commercialBr.push(new Array());
+      prNames.push(
+        new Array(
+          fetchedData[j].show.title,
+          descr[j].description.short,
+          fetchedData[j].title,
+          fetchedData[j].duration
+        )
+      );
+    } else {
+      commercialBr.push(new Array(fetchedData[j].timeline_break));
+      prNames.push(
+        new Array(
+          fetchedData[j].show.title,
+          descr[j].description.short,
+          fetchedData[j].title,
+          fetchedData[j].duration
+        )
+      );
+    }
+  }
+
+  const updateOptionsForPrNames = {
+    spreadsheetId: sheetID,
+    range: sheetName + '!D' + row,
+    valueInputOption: 'USER_ENTERED',
+    resource: { values: prNames },
+  };
+
+  const updateOptionsForCommercialBr = {
+    spreadsheetId: sheetID,
+    range: sheetName + '!A' + row,
+    valueInputOption: 'USER_ENTERED',
+    resource: { values: commercialBr },
+  };
+
+  await gsapi.spreadsheets.values.update(updateOptionsForPrNames);
+  await gsapi.spreadsheets.values.update(updateOptionsForCommercialBr);
+}
+
+module.exports = { insertDataFromDB2YourHopeIntoSpreadSheet, fetchAfterEdit };
--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 require('@google-cloud/debug-agent').start({ serviceContext: { enableCanary: false } });
 const express = require('express');
 const { google } = require('googleapis');
-const keys = require('../config/keys.json');
 const moment = require('moment-timezone');
+const keys = require('../config/keys.json');
 const db = require('./utils/db.js');
-const insertDataFromDB2YourHopeIntooSpreadSheet = require('./insertDataIntoSpreadSheets.js');
+const {
+  insertDataFromDB2YourHopeIntooSpreadSheet,
+  fetchAfterEdit,
+} = require('./insertDataIntoSpreadSheets.js');
 const { getYYYmmDD, getDataFromSpreadSheet } = require('./utils/dataFromSheets.js');
 const fetchPrName = require('./utils/fetchPrName');
 
@@ -13,112 +16,117 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 
 const client = new google.auth.JWT(keys.client_email, null, keys.private_key, [
-	'https://www.googleapis.com/auth/spreadsheets',
+  'https://www.googleapis.com/auth/spreadsheets',
 ]);
 
 app.get('/', async (req, res) => {
-	res.send('Created by Antonyuk Bogdan for HopeUA');
+  res.send('Created by Antonyuk Bogdan for HopeUA');
 });
 
 app.post('/fetchFromArchive', async (req, res) => {
-	await insertDataFromDB2YourHopeIntooSpreadSheet(client, req.body.sheetId, req.body.sheetName);
+  await insertDataFromDB2YourHopeIntooSpreadSheet(client, req.body.sheetId, req.body.sheetName);
 
-	res.send('Success!');
+  res.send('Success!');
 });
 
 app.post('/upload', async (req, res) => {
-	const regExp = new RegExp(/^[A-Z]{4}/, 'g');
-	let dateFromSheet = await getYYYmmDD(client, req.body.sheetId, req.body.sheetName); // fetching date from Sheet (2020-07-13)
-	let data = await getDataFromSpreadSheet(client, req.body.sheetId, req.body.sheetName); // fetching all necessary data from Sheet
-	data.map(async function (e) {
-		try {
-			let documentName = dateFromSheet + 'T' + e[1] + ':00';
-			let letters = e[0].match(regExp)[0]; // extract only letters (EPLU03219 => EPLU)
-			let prDate = moment.tz(dateFromSheet + 'T' + e[1] + ':00', 'Europe/Kiev').toDate();
+  const regExp = new RegExp(/^[A-Z]{4}/, 'g');
+  let dateFromSheet = await getYYYmmDD(client, req.body.sheetId, req.body.sheetName); // fetching date from Sheet (2020-07-13)
+  let data = await getDataFromSpreadSheet(client, req.body.sheetId, req.body.sheetName); // fetching all necessary data from Sheet
+  data.map(async function (e) {
+    try {
+      let documentName = dateFromSheet + 'T' + e[1] + ':00';
+      let letters = e[0].match(regExp)[0]; // extract only letters (EPLU03219 => EPLU)
+      let prDate = moment.tz(dateFromSheet + 'T' + e[1] + ':00', 'Europe/Kiev').toDate();
 
-			if (e[2] === 'Episode not found') {
-				const fetchedData = await fetchPrName(letters);
-				e[2] = fetchedData.title;
-				e[3] = fetchedData.description.short;
-				e[4] = null;
-			}
-			ref = await db
-				.collection('entities')
-				.doc(documentName)
-				.set({
-					id: documentName, //uses id: for avoid duplicates and rewrites entity when same
-					date: prDate,
-					show: { uid: letters, title: e[2], description: { short: e[3] }, code: letters },
-					episode: { uid: e[0], title: e[4], description: e[3], language: null, code: e[0] },
-				});
-		} catch (e) {
-			if (e) console.log(e);
-		}
-	});
+      if (e[2] === 'Episode not found') {
+        const fetchedData = await fetchPrName(letters);
+        e[2] = fetchedData.title;
+        e[3] = fetchedData.description.short;
+        e[4] = null;
+      }
+      ref = await db
+        .collection('entities')
+        .doc(documentName)
+        .set({
+          id: documentName, //uses id: for avoid duplicates and rewrites entity when same
+          date: prDate,
+          show: { uid: letters, title: e[2], description: { short: e[3] }, code: letters },
+          episode: { uid: e[0], title: e[4], description: e[3], language: null, code: e[0] },
+        });
+    } catch (e) {
+      if (e) console.log(e);
+    }
+  });
 
-	res.status(200).send();
+  res.status(200).send();
+});
+
+app.post('/update', async (req, res) => {
+  await fetchAfterEdit(req.body.sheetId, req.body.sheetName, req.body.range);
+  res.status(200).send();
 });
 
 app.get('/events', async (req, res) => {
-	let startDate = new Date(req.query.date);
-	let endDate = new Date(req.query.dateEnd);
-	const completeData = { data: [] };
-	if (!req.query.date && !req.query.dateEnd) {
-		const snapshot = await db
-			.collection('entities')
-			.where('date', '>=', new Date())
-			.limit(50)
-			.orderBy('date')
-			.get();
-		snapshot.forEach((doc) => {
-			let item = doc.data();
-			item.date = item.date.toDate();
-			completeData.data.push(item);
-		});
-		res.send(completeData);
-	} else if (!req.query.date && req.query.dateEnd) {
-		const snapshot = await db
-			.collection('entities')
-			.where('date', '<=', endDate)
-			.limit(50)
-			.orderBy('date')
-			.get();
-		snapshot.forEach((doc) => {
-			let item = doc.data();
-			item.date = item.date.toDate();
-			completeData.data.push(item);
-		});
-		res.send(completeData);
-	} else if (req.query.date && !req.query.dateEnd) {
-		const snapshot = await db
-			.collection('entities')
-			.where('date', '>=', startDate)
-			.limit(50)
-			.orderBy('date')
-			.get();
-		snapshot.forEach((doc) => {
-			let item = doc.data();
-			item.date = item.date.toDate();
-			completeData.data.push(item);
-		});
-		res.send(completeData);
-	} else {
-		const snapshot = await db
-			.collection('entities')
-			.where('date', '>=', startDate)
-			.where('date', '<=', endDate)
-			.orderBy('date')
-			.get();
-		snapshot.forEach((doc) => {
-			let item = doc.data();
-			item.date = item.date.toDate();
-			completeData.data.push(item);
-		});
-		res.send(completeData);
-	}
+  let startDate = new Date(req.query.date);
+  let endDate = new Date(req.query.dateEnd);
+  const completeData = { data: [] };
+  if (!req.query.date && !req.query.dateEnd) {
+    const snapshot = await db
+      .collection('entities')
+      .where('date', '>=', new Date())
+      .limit(50)
+      .orderBy('date')
+      .get();
+    snapshot.forEach(doc => {
+      let item = doc.data();
+      item.date = item.date.toDate();
+      completeData.data.push(item);
+    });
+    res.send(completeData);
+  } else if (!req.query.date && req.query.dateEnd) {
+    const snapshot = await db
+      .collection('entities')
+      .where('date', '<=', endDate)
+      .limit(50)
+      .orderBy('date')
+      .get();
+    snapshot.forEach(doc => {
+      let item = doc.data();
+      item.date = item.date.toDate();
+      completeData.data.push(item);
+    });
+    res.send(completeData);
+  } else if (req.query.date && !req.query.dateEnd) {
+    const snapshot = await db
+      .collection('entities')
+      .where('date', '>=', startDate)
+      .limit(50)
+      .orderBy('date')
+      .get();
+    snapshot.forEach(doc => {
+      let item = doc.data();
+      item.date = item.date.toDate();
+      completeData.data.push(item);
+    });
+    res.send(completeData);
+  } else {
+    const snapshot = await db
+      .collection('entities')
+      .where('date', '>=', startDate)
+      .where('date', '<=', endDate)
+      .orderBy('date')
+      .get();
+    snapshot.forEach(doc => {
+      let item = doc.data();
+      item.date = item.date.toDate();
+      completeData.data.push(item);
+    });
+    res.send(completeData);
+  }
 });
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-	console.log(`Server listening on port ${PORT}...`);
+  console.log(`Server listening on port ${PORT}...`);
 });
